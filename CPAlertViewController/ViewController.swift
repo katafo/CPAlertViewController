63a896eb54a7c4b275ba6d950e08bb562195b4f3
Cao Phong <phong.caothanh@apps-cyclone.com>
2017-02-19 15:20:03 +0700

    Initial Commit

---
+++ CPAlertViewController/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CPAlertViewController
+//
+//  Created by Phong Cao on 2/19/17.
+//  Copyright © 2017 admin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
